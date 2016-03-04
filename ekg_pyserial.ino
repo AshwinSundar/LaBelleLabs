bbff8de33f3d484a0517302b93b913f1c69f1442
AshwinSundar <ashiundar@gmail.com>
2016-03-03 17:45:35 -0700

    ekg_pyserial.ino
    Initial commit of ekg_pyserial.ino. Just grabs the analog input from the EKG and plots it in pyserial. Could use refinement.

---
+++ ekg_pyserial.ino
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+
+int analogPin = 0;
+float analogEkg, voltageEkg;
+
+void setup()
+{
+  
+  Serial.begin(115200);
+  
+}
+
+void loop()
+{
+  
+  analogEkg = analogRead(analogPin);
+  voltageEkg = analogEkg*5.0/1024.0;
+  
+  
+  
+  Serial.print("EKGData:");
+  Serial.print(voltageEkg);
+  Serial.println(":");
+  delay(10); // sampling rate = 5ms ~ 200Hz
+
+
+ 
+}
+
+
+
