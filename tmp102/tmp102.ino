d3fe3cd6c90d24c4f1a5c1369944d8b85f73afdc
Ashwin Sundar <ashiundar@gmail.com>
2016-03-14 22:50:16 -0700

    Did some major optimization
    Removed Serial commands because we are not communicating via Serial.
    Optimized code for speed by removing unnecessary lines. Removed
    delay(1000) in setup. Removed fahrenheit conversion. Created
    placeholder for bluetooth communications in the loop. Minor edit to
    point 3 in best practices section.

--- tmp102/tmp102.ino
+++ tmp102/tmp102.ino
@@ -6,50 +6,38 @@
 // Name of Editor: Ashwin Sundar
 // Date of GitHub commit: March 14, 2016
 // What specific changes were made to this code, compared to the currently up-to-date code
-// on GitHub?: Cleaned up the code. Removed unneccessary variables. Rewrote any variables 
-// that had unclear names
+// on GitHub?: Removed Serial commands because we are not communicating via Serial. 
+// Optimized code for speed by removing unnecessary lines. Removed delay(1000) in setup. 
+// Removed fahrenheit conversion. Created placeholder for bluetooth communications in the loop.  
 //// //// //// //// ////
 // Best coding practices
 // 1) When you create a new variable or function, make it obvious what the variable or
 // function does. The name of the variable or function should reflect its purpose. 
 // 2) Comment copiously. Created a new variable or function? Explain why. Explain what
 // your functions do. Feel free to link to sites that you referenced for information. 
-// 3) Please compile your code, run it on a device, and make sure it is functional BEFORE
-// committing changes to GitHub. 
+// 3) Please compile your code and make sure it is functional BEFORE committing changes to GitHub. 
 //// //// //// //// ////
 //Arduino 1.0+ Only
 //////////////////////////////////////////////////////////////////
-//©2011 bildr
-//Released under the MIT License - Please reuse change and share
-//Simple code for the TMP102, simply prints temperature via serial
-//////////////////////////////////////////////////////////////////
-// extra
 #include <Wire.h>
 int tmp102Address = 0x48;
 
 void setup(){
-  Serial.begin(9600);
   Wire.begin();
-  Serial.println("reset");
 }
 
 void loop(){
-
-  float celsius = getTemperature();
-  Serial.print("Celsius: ");
-  Serial.println(celsius);
-
-
-  float fahrenheit = (1.8 * celsius) + 32;  
-  Serial.print("Fahrenheit: ");
-  Serial.println(fahrenheit);
-
-  delay(1000); //just here to slow down the output. You can remove this
+  // Placeholder for bluetooth communications. I will dump the result of getTemperature
+  // into a bluetooth packet and transmit it to anyone listening. 
+  
 }
 
+
+// Uses Wire library to request temperature from tmp102 module. Returns temperature
+// in Celsius. Precision unknown at this time. 
 float getTemperature(){
   Wire.requestFrom(tmp102Address,2); 
-
+  
   byte MSB = Wire.read();
   byte LSB = Wire.read();
 
