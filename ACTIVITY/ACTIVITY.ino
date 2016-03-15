91c35b2084b8ee9a108ed4a00014df1b0e2aa867
Ashwin Sundar <ashiundar@gmail.com>
2016-03-14 23:05:32 -0700

    Major optimization and commentary
    Updated point 3 of best practices for this particular file. Removed
    setLed command in loop. Optimized initialization of accel variable.
    Inserted minor to-do tasks and commentary. Added placeholder for
    bluetooth communications code.

--- ACTIVITY/ACTIVITY.ino
+++ ACTIVITY/ACTIVITY.ino
@@ -14,8 +14,12 @@
 // function does. The name of the variable or function should reflect its purpose. 
 // 2) Comment copiously. Created a new variable or function? Explain why. Explain what
 // your functions do. Feel free to link to sites that you referenced for information. 
-// 3) Please compile your code, run it on a device, and make sure it is functional BEFORE
-// committing changes to GitHub. 
+// 3) For this particular piece of code, since it is written for the LightBlue Bean, the code
+// WILL NOT compile in the Arduino IDE. You'll get a bunch of error messages about a lot of 
+// functions not being "declared in this scope". That's because those functions only exist in
+// the world of LightBlue Bean. You need to compile this code in BeanLoader, make sure it runs
+// over there, then copypasta it into here, hit Save, and commit the change in GitHub Desktop. 
+// Don't forget to provide a detailed description when you commit a file. 
 //// //// //// //// ////
 
 // Steps are integer values. I initialize step count at 0 when the sketch loads
@@ -28,31 +32,19 @@ void setup() {
 }
 
 void loop() {
-
-    Bean.setLed(0,0,255);
-    AccelerationReading accel = {0, 0, 0};
-    accel = Bean.getAcceleration();
-    
-    //Serial.print("x1 value: ");
-  //  Serial.println(accel.xAxis);
-    //Serial.print("y1 value: ");
-  //  Serial.println(accel.yAxis);
-    //Serial.print("z1 value: ");
-  //  Serial.println(accel.zAxis);
+    AccelerationReading accel = Bean.getAcceleration();
     
     int x1 = accel.xAxis;
     int y1 = accel.yAxis;
     int z1= accel.zAxis;
 
-    Bean.sleep(25);
+// The 25 ms period between acceleration readings is arbitrary from what I understand. 
+// Please provide reasoning as to why 25 ms has been chosen. 
+Bean.sleep(25);
 
     accel = Bean.getAcceleration();
   
     int x2 = accel.xAxis;
-
- //   Serial.print("x2 : ");
- //   Serial.println(x2);
-
     int y2 = accel.yAxis;
     int z2 = accel.zAxis;
 
@@ -60,14 +52,22 @@ void loop() {
     int deltaY = (y2-y1);
     int deltaZ = (z2-z1);
 
-  if (deltaX <= -20 || deltaY <= -20 || deltaZ <= -20) { //deltaY is the most important
+// This algorithm needs improvement. Acceleration is merely a proxy for step count. We need
+// to analyze the data using Professor Spano's EKG algorithm here. The EKG algorithm establishes
+// a baseline and a threshold for any set of periodic data. 
+  if (deltaX <= -20 || deltaY <= -20 || deltaZ <= -20) { 
   
   stepCount = stepCount + 1;
-  Bean.setLed(255,0,0);
+
+// This next line is known as a refractory period. We have arbitrarily picked 25 ms. This 
+// will not do. Please determine an appropriate refractory period for step count by
+// consulting the gait literature provided by Victoria (email Ashwin, he has the literature)
   Bean.sleep(25);
   
 }
 
+// These variables must be dynamically set by the experimenter. They cannot be hard-coded
+// like this. 
 double height = 6*12+2; // inputs INCHES
 double weight = 200; // inputs LBS
 
@@ -76,14 +76,7 @@ double distance = (stride*stepCount)/(1609.34); // miles traveled
 double calories = distance*(0.57*weight); // calories burnt BASED ON LIVESTRONG
 double literature_cal = ((stride*stepCount)/1000)*(weight*0.45359237)*0.7788; //Calories burnt based on literature review
 
-
-Serial.print("Step count: ");
-Serial.println(stepCount);
-Serial.print("Miles performed: ");
-Serial.println(distance);
-Serial.print("Calories burnt: ");
-//Serial.println(calories);
-Serial.println(literature_cal);
+// Once again, a placeholder. Bluetooth communications protocols need to go here. 
  
 Bean.sleep(25);
     
