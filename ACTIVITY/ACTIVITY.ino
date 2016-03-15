0fbdddc6b7ce3980794eeb76ee458b8cf3321e11
Ashwin Sundar <ashiundar@gmail.com>
2016-03-14 19:47:55 -0700

    Setup procedure
    Due to an oddity with the Arduino IDE, all .ino files must be inside of
    a self-titled folder. I made organizational structure changes to
    reflect this requirement.

--- ACTIVITY/ACTIVITY.ino
+++ ACTIVITY/ACTIVITY.ino
@@ -1,5 +1,3 @@
-// edit 2
-// Modified 14 March 2015
 // INSTRUCTIONS FOR EDITOR - THIS SECTION MUST BE COMPLETED
 // Fill out this header section BEFORE attempting to commit changes to GitHub
 // Title of File: ACTIVITY.ino
