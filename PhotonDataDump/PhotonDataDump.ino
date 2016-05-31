488a586e757b7dd2b920898427b5d038c1f71b43
Ashwin Sundar <ashiundar@gmail.com>
2016-05-31 12:02:29 -0700

    Thermistor Readings
    Added some code to read thermistor voltage divider circuit, convert to
    Celsius

--- PhotonDataDump/PhotonDataDump.ino
+++ PhotonDataDump/PhotonDataDump.ino
@@ -4,10 +4,9 @@
 //// //// //// //// ////
 // Title of File: PhotonDataDump.ino
 // Name of Editor: Ashwin Sundar
-// Date of GitHub commit: May 16, 2016
+// Date of GitHub commit: June 1, 2016
 // What specific changes were made to this code, compared to the currently up-to-date code
-// on GitHub?: Removed all extraneous variables, goal is to start from a clean slate. Added
-// in 3 analog read variables just to read data from the ADXL335. 
+// on GitHub?: Added some code to read thermistor voltage divider circuit, convert to Celsius
 //// //// //// //// ////
 // Best coding practices
 // 1) When you create a new variable or function, make it obvious what the variable or
@@ -27,12 +26,17 @@
 // XOut        -  A2
 // Test        -  A3
 
+#include <math.h>
+
 // int's in C++ cannot have numbers in their name. Hence the bizarre nomenclature. 
 int XAccel; // x acceleration from the ADXL335
 int YAccel; // y acceleration from the ADXL335
 int ZAccel; // z acceleration from the ADXL335
-int VOut; // 3v0 output of the ADXL335
-// 
+int VOut; // voltage output of the ADXL335
+//
+double thermistorAnalog; // contains raw analog data from thermistor circuit VOut
+double thermistorResistance; // converts analog reading to resistance
+double temperature; // converts thermistor resistance to temperature, according to Amphenol datasheet 
 int EKG; // analog reading of EKG
 
 void setup()
@@ -49,6 +53,7 @@ void setup()
  Particle.variable("ZAccel", ZAccel);
  Particle.variable("VOut", VOut);
  Particle.variable("EKG", EKG);
+ Particle.variable("temp", temperature); // publishes temperature in Celsius
 }
 
 void loop()
@@ -61,6 +66,11 @@ void loop()
  // determine the reference voltage by querying VOut on A3
  VOut = analogRead(A3);
  
+ // read the raw thermistor analog data
+ thermistorAnalog = analogRead(A5);
+ thermistorResistance = (10000*thermistorAnalog/4096)/(1-thermistorAnalog/4096);
+ // calculated temperature by plotting a line of best fit using linear algebra and MATLAB. Error was found to be 3.7998*10^-4 Kelvins using Igor Pro. 
+ temperature = pow((1.1106*.001 + 2.3724*.0001*log(thermistorResistance) + 7.4738*.00000001*pow(log(thermistorResistance), 3)), -1) - 273.15;
  
  // read the EKG, connected to A4 of the Photon. 
  EKG = analogRead(A4);
