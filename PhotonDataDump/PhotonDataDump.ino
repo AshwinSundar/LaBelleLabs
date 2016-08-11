7dadbae7ed6d546cf28c5971901c132ed6dc8006
Ashwin Sundar <ashiundar@gmail.com>
2016-08-11 16:38:53 -0700

    Started from scratch
    Fully rewrote the code from scratch, unit testing along the way. I'm
    doing
    this because I can't figure out what's wrong with the July 6 code that
    is creating strange hardware faults and SD errors.. Unit testing the
    writeToSD() function tonight.

--- PhotonDataDump/PhotonDataDump.ino
+++ PhotonDataDump/PhotonDataDump.ino
@@ -4,9 +4,12 @@
 //// //// //// //// ////
 // Title of File: PhotonDataDump.ino
 // Name of Editor: Ashwin Sundar
-// Date of GitHub commit: August 4, 2016
+// Date of GitHub commit: August 10, 2016
 // What specific changes were made to this code, compared to the currently up-to-date code
-// on GitHub?: Commented out .publish()'s, turned off WiFi for power testing. 
+// on GitHub?: Fully rewrote the code from scratch, unit testing along the way. I'm doing
+// this because I can't figure out what's wrong with the July 6 code that is creating. Unit
+// testing the writeToSD() function tonight.
+// strange hardware faults and SD errors. 
 //// //// //// //// ////
 // Best coding practices
 // 1) When you create a new variable or function, make it obvious what the variable or
@@ -17,32 +20,31 @@
 //// //// //// //// ////
 //// //// //// //// ////
 // Particle Photon connections to ADXL335
-// Photon     <-> ADXL335 Breakout Board
-// 3v3         -  Vin
-// 3Vo         -  A3
-// GND         -  GND
-// ZOut        -  A0
-// YOut        -  A1
-// XOut        -  A2
-// Test        -  A3
+// Photon     <->   ADXL335 Breakout Board
+// 3v3         -    Vin
+// 3Vo         -    A3
+// GND         -    GND
+// ZOut        -    A0
+// YOut        -    A1
+// XOut        -    A2
+// Test/ST     -    N/A
 //// //// //// //// ////
 //// //// //// //// ////
 // Particle Photon connections to microSD breakout board
-// Photon     <-> ADXL335 Breakout Board
-// N/A         -  CD
-// D3          -  D0
-// GND         -  GND
-// D4          -  SCK
-// 3V3         -  VCC
-// D2          -  DI
-// D1          -  CS
+// Photon     <->   ADXL335 Breakout Board
+// N/A         -    CD
+// D3          -    D0
+// GND         -    GND
+// D4          -    SCK
+// 3V3         -    VCC
+// D2          -    DI
+// D1          -    CS
 //// //// //// //// ////
 //// //// //// //// ////
 
-#include <math.h>
-#include <Particle.h>
 // This #include statement was automatically added by the Particle IDE.
 #include "SdFat/SdFat.h"
+#include "math.h"
 
 //// //// //// //// ////
 //// microSD config ////
@@ -74,44 +76,25 @@ const uint8_t chipSelect = D0;
 #endif  // SPI_CONFIGURATION
 //// //// //// //// ////
 //// //// //// //// ////
+SYSTEM_MODE(SEMI_AUTOMATIC); // allows Photon code to execute without being connected
+// to a WiFi network. You must manually call Particle.connect(). In AUTOMATIC mode, 
+// Particle.connect() is automatically called before any code is executed, and the 
+// device waits to be connected to WiFi before executing any of your code. 
 
-// note: ints in C++ cannot have numbers in their name.
-double thermistorAnalog; // contains raw analog data from thermistor circuit VOut
-double thermistorResistance; // converts analog reading to resistance
-double temperature; // converts thermistor resistance to temperature, according to Amphenol datasheet 
-String accelData; // ADXL335 acceleration magnitude data points
-int EKG = 0; // analog reading of EKG
-String accel;
-int xaccel = 0; // analog reading of x acceleration
-int yaccel = 0; // analog reading of y acceleration
-int zaccel = 0; // analog reading of z acceleration
-int lastTempCheck = 0;
-int lastEKGCheck = 0; 
-int lastAccelCheck = 0;
-String EKGData; // collection of EKG points, to be published in bulk
-
-File globalTemp; // for microSD interface
-File globalEKG;
-File globalAccel;
-
-Timer EKGTimer(10, checkEKG); // creates timer that checks EKG every .01 seconds
-Timer TempTimer(5000, checkTemp); // creates timer that checks temperature every 5 seconds
-Timer AccelTimer(200, checkAccel); // creates timer that checks acceleration every 0.2 seconds
-    
+double start;
+double stop;
+File testFile; 
 void setup()
 {
-    WiFi.off();
-    RGB.control(true);
-    RGB.color(0,2,0); // to confirm that WiFi is off
-    pinMode(D7, OUTPUT);
-    Particle.syncTime(); // syncs time to correct time zone
-    EKGTimer.start(); // by default, timers are stopped. start them. 
-    TempTimer.start();
-    AccelTimer.start();
+    Particle.connect();
+    Serial.begin(115200);
+    // RGB.control(true);
+    // RGB.color(255, 255, 0);
     // Initialize SdFat or print a detailed error message and halt
     // Use half speed like the native library.
     // Change to SPI_FULL_SPEED for better performance.
-    if (!sd.begin(chipSelect, SPI_HALF_SPEED)) 
+    start = micros();
+    if (!sd.begin(chipSelect, SPI_FULL_SPEED)) 
     {
         sd.initErrorHalt();
     }
@@ -119,166 +102,49 @@ void setup()
     // prepare files for writing. O_RDWR enables read-write, O_CREAT makes a new file
     // if there isn't already a file with the name in quotes, O_AT_END adds new data
     // to the end of the file instead of overwriting the contents. 
-    if (!globalTemp.open("globalTemp.txt", O_RDWR | O_CREAT | O_AT_END)) 
+    if (testFile.open("testFile.txt", O_RDWR | O_CREAT | O_AT_END)) 
     {
-        sd.errorHalt("SETUP(): opening globalTemp.txt for write failed"); // debug info
-    }
-    else 
-    { 
-        globalTemp.close();
+        Serial.print("testFile.txt opened successfully.");
     }
     
-    if (!globalEKG.open("globalEKG.txt", O_RDWR | O_CREAT | O_AT_END))
-    {
-        sd.errorHalt("SETUP(): opening globalEKG.txt for write failed"); // debug info
-    }
     else 
     { 
-        globalEKG.close();
+        Serial.print("setup(): opening testFile.txt for 0_RDWR | 0_CREAT | 0_AT_END.");
     }
+
+    testFile.close(); 
     
-    if (!globalAccel.open("globalAccel.txt", O_RDWR | O_CREAT | O_AT_END))
-    {
-        sd.errorHalt("SETUP(): opening globalAccel.txt for write failed"); // debug info
-    }
-    else 
-    { 
-        globalAccel.close();
-    }
+    stop = micros(); 
+    
+    Serial.print("Time to execute setup(): " );
+    Serial.print((stop - start)/1000);
+    Serial.print(" milliseconds; ");
 }
 
 void loop()
 {
-    
+    writeToFile();
 }
 
-void checkTemp()
-{
-    if (!globalTemp.open("globalTemp.txt", O_RDWR | O_CREAT | O_AT_END)) 
+void writeToFile() {
+    start = micros();
+    if (testFile.open("testFile.txt", O_RDWR | O_AT_END)) 
     {
-        sd.errorHalt("CHECKTEMP(): opening globalTemp.txt for write failed"); // debug info
-    }
-    thermistorAnalog = analogRead(A5); // read the raw thermistor analog data
-
-    // convert resistance to ohms using voltage divider equation, where R1 = 10k ohms
-    // shielded equation from dividing by zero by putting it in an if statement
-    if (thermistorAnalog < 4095)
-    {
-        thermistorResistance = (9820*thermistorAnalog/4095)/(1-thermistorAnalog/4095);
+        Serial.print("Opening testFile.txt succeeded. "); // debug info
+        testFile.print("gibberish");
+        stop = micros();
+        Serial.print("Time to execute open and print: " );
+        Serial.print((stop - start)/1000); 
+        Serial.println(" milliseconds;");
     }
     
-    else if (thermistorAnalog >= 4095) // if the analog reads 4095, then there is a short or the thermistor has failed.
-    {
-        thermistorResistance = 0; 
+    else
+    { 
+        Serial.print("writeToFile(): opening testFile.txt for 0_RDWR | 0_AT_END failed. ");
+        
     }
-    // calculated temperature by plotting a line of best fit using linear algebra and MATLAB. Error was found to be 3.7998*10^-4 Kelvins 
-    // using Igor Pro. Compressed some of the math operations to reduce floating point error. 
-    temperature = pow((.0011106 + 0.00023724*log(thermistorResistance) + 0.000000074738*pow(log(thermistorResistance), 3)), -1) - 273.15;
-    // rounds the temperature to 2 decimals, truncates last 6 elements (trailing zeroes))
-    // Particle.publish only accepts chars or strings
-    Serial.print("Writing to globalTemp.txt..."); // serial debug
-    globalTemp.print(Time.year()); // prints formatted time
-    globalTemp.print(", ");
-    globalTemp.print(Time.month());
-    globalTemp.print(", ");
-    globalTemp.print(Time.day());
-    globalTemp.print(", ");
-    globalTemp.print(Time.hourFormat12());
-    globalTemp.print(", ");
-    globalTemp.print(Time.minute());
-    globalTemp.print(", ");
-    globalTemp.print(Time.second());
-    globalTemp.print(", ");
-    globalTemp.print(String(floor(temperature*100 + 0.5)/100).remove(5)); // writes temp
-    globalTemp.println("; ");
     
-    // commented out until we find a WiFi solution
-    // Particle.publish("newTemp", String(floor(temperature*100 + 0.5)/100).remove(5)); 
-    
-    globalTemp.close();
+    testFile.close();
 }
 
-void checkEKG()
-{
-    if (!globalEKG.open("globalEKG.txt", O_RDWR | O_CREAT | O_AT_END)) 
-    {
-        sd.errorHalt("CHECKEKG(): opening globalEKG.txt for write failed"); // debug info
-    }
-    
-    EKG = analogRead(A4); // read the EKG, connected to A4 of the Photon. 
-    EKGData += String(EKG); 
-    EKGData += ",";
-    
-    // commented out until we find a WiFi solution    
-    // if (EKGData.length() > 250)
-    // {
-    //     Particle.publish("EKG", EKGData);
-    //     EKGData = ""; // erases the EKG data buffer, starts over
-    // }
-    
-    // Serial.print("Writing to globalEKG.txt..."); // serial debug
-    globalEKG.print(Time.year()); // prints formatted time
-    globalEKG.print(", ");
-    globalEKG.print(Time.month());
-    globalEKG.print(", ");
-    globalEKG.print(Time.day());
-    globalEKG.print(", ");
-    globalEKG.print(Time.hourFormat12());
-    globalEKG.print(", ");
-    globalEKG.print(Time.minute());
-    globalEKG.print(", ");
-    globalEKG.print(Time.second());
-    globalEKG.print(", ");
-    globalEKG.print(String(EKG)); // writes EKG
-    globalEKG.println("; ");
-    
-    globalEKG.close();
-}
 
-void checkAccel()
-{
-    if (!globalAccel.open("globalAccel.txt", O_RDWR | O_CREAT | O_AT_END)) 
-    {
-        sd.errorHalt("CHECKACCEL(): opening globalAccel.txt for write failed"); // debug info
-    }
-    
-    // X, Y, and Z are directly wired to A2, A1, and A0 respectively. Rounds data, truncates trailing zeros and decimal. 
-    xaccel = analogRead(A0);
-    yaccel = analogRead(A1);
-    zaccel = analogRead(A2);
-    accel = String(floor(sqrt(pow(xaccel,2) + pow(yaccel,2) + pow(zaccel,2)) + 0.5)).remove(4); 
-    accelData += accel; 
-    accelData += ",";
-    
-    // commented out until we find a WiFi solution
-    // if (accelData.length() > 250)
-    // {
-    //     Particle.publish("Acceleration", accelData);
-    //     accelData = ""; // erases the acceleration data buffer, starts over
-    // }
-    
-    // Serial.print("Writing to globalAccel.txt..."); // serial debug
-    globalAccel.print(Time.year()); // prints formatted time
-    globalAccel.print(", ");
-    globalAccel.print(Time.month());
-    globalAccel.print(", ");
-    globalAccel.print(Time.day());
-    globalAccel.print(", ");
-    globalAccel.print(Time.hourFormat12());
-    globalAccel.print(", ");
-    globalAccel.print(Time.minute());
-    globalAccel.print(", ");
-    globalAccel.print(Time.second());
-    globalAccel.print(", ");
-    globalAccel.print("xaccel: ");
-    globalAccel.print(String(xaccel)); // writes x acceleration data
-    globalAccel.print(", ");
-    globalAccel.print("yaccel: ");
-    globalAccel.print(String(yaccel)); // writes y acceleration data
-    globalAccel.print(", ");
-    globalAccel.print("zaccel: ");
-    globalAccel.print(String(zaccel)); // writes z acceleration data
-    globalAccel.println("; ");
-    
-    globalAccel.close();
-}
